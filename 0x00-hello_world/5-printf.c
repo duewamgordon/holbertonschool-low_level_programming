db331f1e2d597551ecc69df5097f798c7ae851ce
root <duewamgordon@gmail.com>
2022-09-16 08:28:28 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Print and compile
+ *
+ * Return: Always 0 (Success)
+ */
+gcc -Wall 5-printf.c -o printf
+
+int main(void)
+{
+	printf("\n"with proper grammar, but the outcome is a piece of art,");
+	return (0)
+}
