b59200ebf7be57fbe728b348a5e13d554d22ad33
root <duewamgordon@gmail.com>
2022-09-16 08:50:44 -0700

    print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,8 +5,8 @@
  *
  * Return: Always 0 (Success)
  */
-gcc -Wall 5-printf.c -o printf
+
 int main(void)
 {
-	printf("\n"with proper grammar, but the outcome is ap ience of art,"); 
-		return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n"); 
+		return (0);}
