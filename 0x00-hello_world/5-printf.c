d9c5505592529f5a1bec54156ef44bdfc10400f4
root <duewamgordon@gmail.com>
2022-09-16 08:41:04 -0700

    puts

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Print and compile
+ * main-Print and compile
  *
  * Return: Always 0 (Success)
  */
 gcc -Wall 5-printf.c -o printf
-
 int main(void)
 {
-	printf("\n"with proper grammar, but the outcome is a piece of art,");
-	return (0)
-}
+	printf("\n"with proper grammar, but the outcome is ap ience of art,"); 
+		return (0);
