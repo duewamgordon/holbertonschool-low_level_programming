920a0bab48f649ae5b7b38b58c48003e647097a3
root <duewamgordon@gmail.com>
2022-09-19 20:24:52 -0700

    printcomb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Print single digit numbers all combinations
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	int c;
+
+	for (c= '0'; c <= '9'; c++)
+	{
+		putchar(C);
+
+		if (c != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
