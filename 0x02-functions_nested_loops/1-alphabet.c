d89c9a224147a66d9e4953bf6f0489d5e0a7e1b4
root <duewamgordon@gmail.com>
2022-09-20 11:33:33 -0700

    putchar

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,13 +5,12 @@
  *
  * Return: Always (0)
  */
-int main(void)
+void print_alphabet(void)
 {
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
-		_putchar(letter);
+		p::utchar(letter);
 
 	putchar('\n');
-	return (0);
 }
