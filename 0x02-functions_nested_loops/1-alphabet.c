0b9533e9e681a71300ec98b534bc15a630c5fe29
root <duewamgordon@gmail.com>
2022-09-20 10:32:03 -0700

    putchar

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,14 +3,14 @@
 /**
  * main- Print lowercase alphabet
  *
- * Return: Always (0
+ * Return: Always (0)
  */
 int main(void)
 {
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
-		putchar(letter);
+		_putchar(letter);
 
 	putchar('\n');
 	return (0);
