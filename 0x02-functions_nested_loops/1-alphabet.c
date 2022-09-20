adf163377d78c050400769f871a03b52d260b226
root <duewamgordon@gmail.com>
2022-09-20 11:46:44 -0700

    nested

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,12 +5,12 @@
  *
  * Return: Always (0)
  */
-void print_alphabet(void)
+int main()
 {
-	char letter;
-
-	for (letter = 'a'; letter <= 'z'; letter++);
-		putchar(letter);
+	int ch;
+	
+	for (ch = 'a'; ch <= 'z'; ch++);
+		putchar(ch);
 
 	putchar('\n');
 }
