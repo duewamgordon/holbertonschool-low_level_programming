506f98e534b2ebdb3fc9c44b97ae5d602a44d2de
root <duewamgordon@gmail.com>
2022-09-22 12:15:45 -0700

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -9,9 +9,11 @@
 int _islower(int c)
 	
 {
-	if((c => 'a') && (c <= 'z')
-			{ 
-			return(1);
-			}
-			return(0);
-			}
+	if ((c => 'a') && (c <= 'z'))
+		
+	{
+		return(1);
+	}
+	
+	return(0);
+}
