51fd7144db698d799887bc698dfb813eff1b0a8b
root <duewamgordon@gmail.com>
2022-09-22 12:56:26 -0700

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isalpha - Checks for alphabetic character
+ *
+ * Returns: 1 if c is a letter otherwise returns 0
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a')) || (c <= 'z'))
+	{
+		return (1);
+	}
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -8,4 +8,5 @@ int _putchar(char c);
 void print_alphabet_x10(void);
 int _putchar(char c);
 int _islower(int c);
+int _isalpha(int c);
 #endif
