c296c83681972adcf7562c96718b54067a75af7a
root <duewamgordon@gmail.com>
2022-09-22 19:30:33 -0700

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,12 +2,13 @@
 
 /**
  * _isalpha - Checks for alphabet
+ *@c: The character to be checked
  *
  * Return: 1 for alphabet or 0 for anything else
  */
 int _isalpha(int c)
 {
-	if((c >= 'A' && c <='Z') || (c <= 'a' && c <= 'z'))
+	if ((c >= 'A' && c <= 'Z') || (c <= 'a' && c <= 'z'))
 	{
 		return (1);
 	}
