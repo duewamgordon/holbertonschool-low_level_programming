5b22a48de4c8840d79d6ade7a1cfce30d6f1a88d
root <duewamgordon@gmail.com>
2022-10-03 07:48:37 -0700

    task 0

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,8 +1,12 @@
-#ifndef MAIN_H                                                                  # define MAIN_H                                                                 /**                                                                              * Function Prototypes                                                           */   
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * Function Prototypes
+ */
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
-int void print_numbers(void);
+void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
