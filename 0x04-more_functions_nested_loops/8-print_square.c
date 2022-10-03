d5fa8a96dc6216df8281b86b6ff7267b7a6be187
root <duewamgordon@gmail.com>
2022-10-03 12:14:19 -0700

    printsquare

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_square - Prints squares followed by a new line
+ * @size: Number of squares/number of times
+ *
+ * Return: empty
+ */
+void print_square(int size)
+{
+	int a, b;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (a = 0; a < size; a++)
+		{
+			for (b = 0; b < size; b++)
+			{
+				_putchar (35);
+			}
+
+			_putchar ('\n');
+		}
+	}
+}
