2fddbce6ef487d10af1d16e39cbb597a2a5bcb23
root <duewamgordon@gmail.com>
2022-10-04 16:20:10 -0700

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * _strncat - Concatenate two strings
+ * @dest: first parameter
+ * @src:  second parameter
+ * @n: 3rd parameter
+ *
+ * Return: string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+        _strncat(dest, src, n);
+	return (dest);
+}
