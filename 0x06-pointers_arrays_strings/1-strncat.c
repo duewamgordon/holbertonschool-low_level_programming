040b92b08cb8f23043ab028517ac92c2da79631e
root <duewamgordon@gmail.com>
2022-10-04 16:41:03 -0700

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
- * _strncat - Concatenate two strings
+ * _strncat - concatenate two strings
  * @dest: first parameter
  * @src:  second parameter
  * @n: 3rd parameter
@@ -10,6 +10,6 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-        _strncat(dest, src, n);
+	_strncat(dest, src, n);
 	return (dest);
 }
