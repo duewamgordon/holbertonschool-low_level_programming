bb2968f1eab3231501840a945bbf5a385494fc4e
root <duewamgordon@gmail.com>
2022-10-04 17:11:05 -0700

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,15 +1,30 @@
 #include "holberton.h"
-#include <stdio.h>
+
 /**
  * _strncat - concatenate two strings
- * @dest: first parameter
- * @src:  second parameter
- * @n: 3rd parameter
+ * @dest: destination
+ * @src: source
+ * @n: max number of bites used from source
  *
- * Return: string
+ * Return: A pointer to the resulting string
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	_strncat(dest, src, n);
+	int len = 0, j = 0;
+
+	while (dest[len])
+	{
+		len++;
+	}
+
+	while (j <  n && src[j])
+	{
+		dest[len] = src[j];
+		len++;
+		j++;
+	}
+
+	dest[len + 1] = '\0';
+
 	return (dest);
 }
