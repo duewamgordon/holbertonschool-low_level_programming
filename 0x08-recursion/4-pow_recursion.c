ca4b9849e9910bcf3bb68282bb03ebae7f137ea4
root <duewamgordon@gmail.com>
2022-10-23 16:20:37 -0700

    powrecursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -5,12 +5,14 @@
  * @x: integer
  * @y: exponent
  *
- * Return: -1
+ * Return: value multiplied
  */
 int _pow_recursion(int x, int y)
 {
-	if (y == 0)
+	if (y < 0)
 		return (-1);
-	else
-		return (x * _pow_recursion(x, y - 1));
+
+	if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
 }
