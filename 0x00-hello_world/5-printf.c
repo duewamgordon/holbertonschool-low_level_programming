36d29ee62148485000f58204c0c7220edb84e25c
duewamgordon <duewamgordon@gmail.com>
2023-01-29 08:14:03 -0800

    printfc

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ * print - print "with proper grammar, but the outcome is a piece of art"
+ * Return: (0).
+ **/
+
+int main()
+{
+	printf("\"with proper grammar, but the outcome is a piece of art");
+	Return(0);
+}
