aec372d90cc6ea8d8cf062ea38063f9d0928d42a
duewamgordon <duewamgordon@gmail.com>
2023-01-29 10:10:12 -0800

    sizec

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,17 +6,11 @@
 
 int main(void)
 {
-	char charType;
-	int intType;
-	long int longintType;
-	long long int longlongintType;
-	float floatType;
-	
 	printf("Size of a char: %zu byte\n", sizeof(char));
 	printf("Size of an int: %zu bytes\n", sizeof(int));
 	printf("Size of a long int: %zu bytes\n", sizeof(long int));
 	printf("Size of a long long int: %zu bytes\n", sizeof(long long int));
 	printf("Size of a float: %zu bytes\n", sizeof(float));
-
+	
 	return (0);
 }
