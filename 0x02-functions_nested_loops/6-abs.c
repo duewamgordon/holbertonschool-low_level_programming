fb088aedf036e327e72cb6f52e97069886e105a4
duewamgordon <duewamgordon@gmail.com>
2023-02-02 11:04:40 -0800

    absc

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"i
+
+/**
+ * _abs - Write a function that computes the abs value of an integer
+ * @c: Value of the integer
+ * Return: Absolute value
+ **/
+
+int _abs(int c)
+{
+	if (c < 0)
+	{
+	int abs_val;
+
+	abs_val = c * -1;
+	return (abs_val);
+	}
+	return (c);
+}
