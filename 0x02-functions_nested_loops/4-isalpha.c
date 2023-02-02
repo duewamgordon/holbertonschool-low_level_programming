9c58aa1dbf29d6338f34eabeef6087baf044bf1e
duewamgordon <duewamgordon@gmail.com>
2023-02-02 10:24:23 -0800

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,14 +3,15 @@
 /**
  * _isalpha - Checks for alphabetic character
  * @c: character checked
- * Returns: 1 if c is an upper or lowercase letter, else 0
+ * Description: returns 1 if is an upper or lowercase letter
+ * Return: 1 or 0
  **/
 
 int _isalpha(int c)
 {
-	if ((c >= 65 &&c <= 90) || (c >= 97 && c <= 122))
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
 	{
 	return (1);
 	}
 	return (0);
-}	
+}
