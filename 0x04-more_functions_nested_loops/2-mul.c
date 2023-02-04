07ade909ad4ec5f9b82af8502eadafe9bdff1c3f
duewamgordon <duewamgordon@gmail.com>
2023-02-04 15:26:45 -0800

    mul

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,6 +2,7 @@
 
 /**
  * mul - Write a function that multiplies two integers
+ * description - multiplier
  * @a: the first integer to be multiplied
  * @b: the second integer to be multiplied
  * Return: 0
@@ -9,12 +10,5 @@
 
 int mul(int a, int b)
 {
-	if (b == 0)
-	return (0);
-	if (b > 0)
-	return (b + mul(a, b - 1));
-	if (b < 0)
-	return (-mul(a, -b));
-	else
-	return (0);
+	return (a * b);
 }
