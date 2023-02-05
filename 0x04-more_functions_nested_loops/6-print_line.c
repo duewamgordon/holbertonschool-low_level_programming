f05c964e8f24de9179497f129fe796fcf84c12ef
duewamgordon <duewamgordon@gmail.com>
2023-02-05 10:26:21 -0800

    printline

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_line - Prints a straight line
+ * @n: the number of Xs the character is printed
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	i = 0;
+	if (n > 0)
+	{
+	while (i > n)
+	{
+	_putchar('_');
+	i++;
+	}
+	_putchar('\n');
+	}
+}
