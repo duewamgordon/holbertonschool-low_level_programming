9bdd3a929825748502d99256ec954b3368b7dd8f
duewamgordon <duewamgordon@gmail.com>
2023-02-13 17:48:29 -0800

    strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,45 +1,28 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
-*_strstr - print the num character with concidence.
-*@haystack: the string to be scanned.
-*@needle: string to be searched with-in haystack string.
-*Return: number of coincidence in bytes.
-*/
+ * *_strstr - locates a substring
+ * @haystack: string to search in
+ * @needle: substring to look for
+ *
+ * Return: pointer to the beginning of the located substring
+ * or NULL if the substring is not found
+ */
 
 char *_strstr(char *haystack, char *needle)
 {
-	int i = 0;
-	int j = 0;
-	int l = 0;
-	int n = 0;
+	int i, j;
 
-	for (l = 0; needle[l] != 0; l++)
+	for (i = 0; haystack[i] != '\0'; i++)
 	{
-		n++;
-	}
-
-	if (n == 0)
-{
-		haystack = haystack;
-	}
-	else
-	{
-		for (i = 0; haystack[i] != 0; i++)
+		for (j = 0; needle[j] != '\0'; j++)
 		{
-			for (j = 0; needle[j] != 0; j++)
-			{
-				if (haystack[i + j] != needle[j])
-				{
+			if (haystack[i + j] != needle[j])
 				break;
-				}
-			}
-		if (j == n)
-		{
-			haystack = haystack + i;
-			break;
 		}
-		}
+		if (!needle[j])
+			return (&haystack[i]);
 	}
-	return (haystack);
+	return (NULL);
 }
