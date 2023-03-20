a8b5d7921294fa1500230a6bbd64c9fe17256a07
duewamgordon <duewamgordon@gmail.com>
2023-03-20 14:45:34 -0700

    addnode

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+
+/**
+ * add_node_end - adds a new node at the end of a list
+ * @head: the head of the list
+ * @str: string that needs to be duplicated
+ * Return: the address of the new element
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new, *temp;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	new->str = strdup(str);
+	new->len = strlen(str);
+	new->next = NULL;
+
+	temp = *head;
+	if (*head == NULL)
+	{
+		*head = new;
+	}
+	else
+	{
+		while (temp->next)
+		{
+			temp = temp->next;
+		}
+		temp->next = new;
+	}
+
+	return (new);
+}
