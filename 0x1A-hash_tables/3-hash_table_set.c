80317d88961673ec33c1c77a3738dfa33b680ade
duewamgordon <duewamgordon@gmail.com>
2023-03-27 21:30:49 -0700

    hashtableset

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,17 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - add element
+ * @key: in the ht
+ * @value: associated to key
+ * @ht: hash table
+ * Return: end program
+ **/
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	(void)key;
+
+	if (!value || !ht)
+		return (0);
+	return (0);
+}
