9f2d500a1177d0102dd2ead1ea987884d0883286
root <duewamgordon@gmail.com>
2023-07-24 10:37:23 -0700

    hashtable6

---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+/**
+* hash_table_delete - deletes a hash table
+* @ht: the hash table
+* Return: void func
+*/
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int k = 0;
+	hash_node_t *node = NULL, *temp = NULL;
+
+	if (!ht)
+		return;
+	for (k = 0; k < ht->size; k++)
+	{
+		node = ht->array[k];
+		while (node)
+		{
+			temp = node->next;
+			free(node->key);
+			free(node->value);
+			free(node);
+			node = temp;
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
