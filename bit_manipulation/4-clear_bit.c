d6ac26226a70e321f2275b777a4a1f99db8522a1
root <duewamgordon@gmail.com>
2023-08-16 18:49:37 -0700

    clearbit

---
+++ bit_manipulation/4-clear_bit.c
@@ -0,0 +1,57 @@
+#include "main.h"
+
+/**
+ * count_bits - count
+ * @n: unsigned long int
+ *
+ * Description: count the number of binaries in int
+ *
+ * Return: unsigned int
+ */
+unsigned int count_bits(unsigned long int n)
+{
+	unsigned int count = 1;
+
+	while (n > 1)
+	{
+		n = n >> 1;
+		count++;
+	}
+
+	return (count);
+}
+
+/**
+ * clear_bit - clear
+ * @n: unsigned long int
+ * @index: unsigned int
+ *
+ * Description: clear the bit on an unsigned long int at a
+ * specific index
+ *
+ * Return: int
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int i = 1;
+	unsigned int count = 0, count_i = 1;
+
+	if (index > 63)
+		return (-1);
+	count = count_bits(*n);
+	while (count_i != count)
+	{
+		if (index == 0)
+			break;
+		i = i << 1;
+		index--;
+		count_i = count_bits(i);
+	}
+
+	if (index == 0)
+	{
+		*n = *n | i;
+		*n = *n ^ i;
+	}
+	return (1);
+}
