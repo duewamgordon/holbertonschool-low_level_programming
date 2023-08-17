ebb8dfa2df24584f338cfc722d403bc2ad35be4f
root <duewamgordon@gmail.com>
2023-08-16 18:27:58 -0700

    printbinary

---
+++ 1-print_binary.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_binary - Prints binary representation of a number
+ * @n: Unsigned int
+ * Return: binary number
+*/
+
+void print_binary(unsigned long int n)
+{
+
+	if (n > 1)
+	{
+		print_binary(n >> 1);
+	}
+
+	if (n & 1)
+	{
+		_putchar('1');
+	}
+	else
+	{
+		_putchar('0');
+	}
+
+}
