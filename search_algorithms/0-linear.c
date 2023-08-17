590c7fae071c6ba91b64a7a991f6811eb2c55502
DuEwa Gordon <duewamgordon@gmail.com>
2023-08-17 11:23:51 -0700

    linear3

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -13,12 +13,12 @@ int linear_search(int *array, size_t size, int value)
 	size_t i;
 	
 	if (array == NULL)
-		return -1;
+		return (-1);
 	for (i = 0; i < size; i++)
 	{
 		printf("Value checked array[%u] = [%d]\n", (unsigned int)i, array[i]);
 		if (array[i] == value)
-			return (int)(i);
+			return (int)( i);
 	}
 	return (-1);
 }
